b1aee4d9519a2f11e8a950e99309971cf178add9
Kaushal sahu <kaushal.r.sahu@gmail.com>
2024-04-24 18:48:24 +0530

    added ScrollReveal

--- code/js/main.js
+++ code/js/main.js
@@ -65,3 +65,19 @@ function scrollUp() {
 	else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
+//ScrollReveal
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400
+})
+
+sr.reveal(`.home-title, .popular-title, .feature-title, .footer-title `);
+sr.reveal(`.home-subtitle, .home-elec, .car-option`, {delay: 450});
+sr.reveal(`.home-img, .popular-section, .all-car, .footer-list`, {delay: 500});
+sr.reveal(`.logos`, {delay: 500, interval: 600});
+sr.reveal(`.home-info, .start-btn`, {delay: 400,interval: 100 , origin: 'bottom'})
+sr.reveal(`.about-img, .map-img, .car-info`, {delay: 400 ,interval: 100 , origin: 'left'})
+sr.reveal(`.about-title, .car-img`, {delay: 400 ,interval: 100 , origin: 'right'})
+sr.reveal(`.card-box`, {delay: 400 ,interval: 200 , origin: 'right'})
